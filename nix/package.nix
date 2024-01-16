4ff49dff9297a9c10c5cc59ae23a430862c3abf0
Aly Chace <achacega@gmail.com>
2024-01-16 15:40:02 -0500

    Renamed binary/package name to pp-to-epp

--- nix/package.nix
+++ nix/package.nix
@@ -1,15 +1,15 @@
 { lib, stdenv, pkgs }:
 
 stdenv.mkDerivation rec {
-  name = "pp-to-amd-epp";
+  name = "pp-to-epp";
 
   propagatedBuildInputs = [ pkgs.power-profiles-daemon pkgs.python3 ];
   unpackPhase = ":";
-  installPhase = "install -m755 -D ${../pp-to-amd-epp} $out/bin/pp-to-amd-epp";
+  installPhase = "install -m755 -D ${../pp-to-epp} $out/bin/pp-to-epp";
 
   meta = {
-    description = "Power Profiles Daemon to AMD P-STATE EPP";
-    mainProgram = "pp-to-amd-epp";
+    description = "Power Profiles Daemon to AMD & Intel P-STATE EPP";
+    mainProgram = "pp-to-epp";
   };
 
 }
